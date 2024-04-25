1cf298260e7b7683d3138d3b3c25c690b2e8ea79
Taha Soltani <tahasoltani766@gmail.com>
2024-04-25 13:40:39 +0330

    Completion of the wallet generator section

---
+++ cracker-wallet/wallet_generator.py
@@ -0,0 +1,35 @@
+from bip39_list import bip_39
+import random
+import string
+
+def generate_random_address(random_words):
+        prefix = random.choice(["bc1", "bc1p"])
+        if prefix == "bc1":
+            initial_suffix = "bc1"
+            possible_characters = string.ascii_lowercase.replace('p', '') + '123456789'
+        elif prefix == "bc1p":
+            initial_suffix = "bc1p"
+            possible_characters = string.ascii_lowercase + '123456789'
+        else:
+            return None
+        suffix_length = 59
+        suffix = ''.join(random.choice(possible_characters) for _ in range(suffix_length))
+        address = initial_suffix + suffix
+        print(address , random_words)
+
+
+def display_random_words(word_list):
+    while True:
+        if len(word_list) < 12:
+            return
+        random_words = random.sample(word_list, 12)
+        generate_random_address(random_words)
+
+
+display_random_words(bip_39)
+
+
+
+
+
+
