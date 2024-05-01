e09866ae4a1ad65ac5b608d48f77e83d03d89ff1
Taha Soltani <tahasoltani766@gmail.com>
2024-05-01 16:16:55 +0330

    Start task api hardware

--- review_target_system/cpu_gpu_model.py
+++ review_target_system/cpu_gpu_model.py
@@ -2,7 +2,6 @@
 # {'python_version': '3.11.9.final.0 (64 bit)', 'cpuinfo_version': [9, 0, 0], 'cpuinfo_version_string': '9.0.0', 'arch': 'X86_64', 'bits': 64, 'count': 12, 'arch_string_raw': 'AMD64', 'vendor_id_raw': 'AuthenticAMD', 'brand_raw': 'AMD Ryzen 5 6600H with Radeon Graphics', 'hz_actual_friendly': '3.3010 GHz', 'hz_actual': [3301000000, 0], 'l2_cache_size': 3145728, 'stepping': 1, 'model': 68, 'family': 25, 'l3_cache_size': 16777216, 'hz_advertised_friendly': '3.2940 GHz', 'hz_advertised': [3294000000, 0], 'flags': ['3dnow', '3dnowext', '3dnowprefetch', 'abm', 'adx', 'aes', 'apic', 'avx', 'avx2', 'bmi1', 'bmi2', 'clflush', 'clflushopt', 'clwb', 'cmov', 'cmp_legacy', 'cr8_legacy', 'cx16', 'cx8', 'de', 'dts', 'erms', 'f16c', 'fma', 'fpu', 'fxsr', 'ht', 'hypervisor', 'ia64', 'invpcid', 'lahf_lm', 'mca', 'mce', 'misalignsse', 'mmx', 'monitor', 'movbe', 'msr', 'mtrr', 'osvw', 'osxsave', 'pae', 'pat', 'pclmulqdq', 'perfctr_core', 'pge', 'pni', 'popcnt', 'pqe', 'pqm', 'pse', 'pse36', 'rdpid', 'rdrnd', 'rdseed', 'sep', 'sepamd', 'serial', 'sha', 'smap', 'smep', 'ss', 'sse', 'sse2', 'sse4_1', 'sse4_2', 'sse4a', 'ssse3', 'tm', 'topoext', 'tsc', 'umip', 'vaes', 'vme', 'vpclmulqdq', 'wdt', 'xsave'], 'l2_cache_line_size': 512, 'l2_cache_associativity': 6} None
 
 import GPUtil
-import cpuinfo
 import multiprocessing
 # OUT PUT :
 # NVIDIA GeForce RTX 3050 Laptop GPU
