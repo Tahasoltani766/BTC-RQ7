01bd2ba85e1e78851e7c835bdb03fccdb8b6a1ae
Taha Soltani <tahasoltani766@gmail.com>
2024-05-01 16:15:38 +0330

    Start task api hardware

--- review_target_system/cpu_gpu_model.py
+++ review_target_system/cpu_gpu_model.py
@@ -1,23 +1,20 @@
 import GPUtil
 import cpuinfo
 import multiprocessing
+# OUT PUT :
+# NVIDIA GeForce RTX 3050 Laptop GPU
 
 def gpu_detection():
     gpus = GPUtil.getGPUs()
     if gpus:
         for i in range(len(gpus)):
             gpu = gpus[i]
-            print("GPU Name:", gpu.name)
+            print(gpu.name)
     else:
         print("No GPU found.")
 
 
-def get_cpu_info():
-    return cpuinfo.get_cpu_info()
-
-
 if __name__ == "__main__":
     multiprocessing.freeze_support()
-    cpu_model = get_cpu_info()
     gpu_model = gpu_detection()
-    print(cpu_model, gpu_model)
+    print(gpu_model)
